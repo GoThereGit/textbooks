49c43dd34f509dd39ea1e52bb2c9ba7d9ceb6132
FlightLessBir <143316970+FlightLessBir@users.noreply.github.com>
2023-08-29 23:23:50 +0800

    Add files via upload

---
+++ test2.php
@@ -0,0 +1,37 @@
+﻿<?php
+print("你好<br>");
+echo"你好";
+echo"你<br>好";
+echo"<font color=red>你好</font>";
+echo"<font color=FF3399>你好</font>";
+echo"<font color=black size=1>你好</font>";
+echo"<font color=black size=10>你好</font>";
+echo"<font face=楷体>你好</font>";
+echo"<font face=宋体>你好</font>";
+
+echo"😓😔😨🤮🤧🤡💩";
+
+echo '<img src="pulpit.jpg" >';
+
+
+<h4>一列:</h4>
+<table border="1">
+  <tr>
+    <td>100</td>
+  </tr>
+</table>
+ 
+<h4>两行三列:</h4>
+<table border="1">
+  <tr>
+    <td>100</td>
+    <td>200</td>
+    <td>300</td>
+  </tr>
+  <tr>
+    <td>400</td>
+    <td>500</td>
+    <td>600</td>
+  </tr>
+</table>
+?>
